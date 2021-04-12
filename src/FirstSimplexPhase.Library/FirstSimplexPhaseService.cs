b2520a3f6715221cc893351ca0703131a7d30d09
hymeck <noonimf@gmail.com>
2021-04-12 14:56:56 +0300

    add preparation actions

--- src/FirstSimplexPhase.Library/FirstSimplexPhaseService.cs
+++ src/FirstSimplexPhase.Library/FirstSimplexPhaseService.cs
@@ -1,38 +1,81 @@
-﻿using MainSimplexPhase.Core;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using MainSimplexPhase.Core;
 using MathNet.Numerics.LinearAlgebra;
 
 namespace FirstSimplexPhase.Library
 {
-    public class FirstSimplexPhaseService
+    public static class FirstSimplexPhaseService
     {
-        public static void MinusOne(ref Matrix<double> matrix, Vector<double> v)
+        private static void MinusOne(ref Matrix<double> matrix, Vector<double> v)
         {
             for (var i = 0; i < v.Count; i++)
                 if (v[i] < 0)
                     matrix.SetRow(i, -matrix.Row(i));
         }
 
-        public static Matrix<double> CreateIdentityMatrix(int size)
+        private static Matrix<double> CreateIdentityMatrix(int size) =>
+            Matrix<double>.Build.DiagonalIdentity(size);
+
+        private static Matrix<double> AppendIdentity(Matrix<double> matrix, Matrix<double> identity) =>
+            matrix.Append(identity);
+
+        private static Vector<double> CreateObjectiveFunctionVector(int columnCount, int rowCount)
         {
-            return Matrix<double>.Build.Diagonal(size, size);
+            var zeros = Enumerable.Repeat<double>(element: 0, count: columnCount); // n of 0's
+            var minusOnes = Enumerable.Repeat<double>(element: -1, count: rowCount); // m of -1's
+            var total = zeros.Concat(minusOnes); // total n + m array
+            return Vector<double>.Build.DenseOfEnumerable(total);
         }
 
-        public static Matrix<double> AppendIdentity(Matrix<double> matrix, Matrix<double> identity)
+        private static Matrix<double> CreateInitialConditionsMatrix(int columnCount, int rowCount)
         {
-            return matrix.Append(identity);
+            var identity = CreateIdentityMatrix(rowCount);
+            var m = Matrix<double>.Build.Dense(rowCount, columnCount); // zero matrix
+            return AppendIdentity(m, identity);
         }
-        
-        // public static (Vector<double> X, ImmutableSortedSet<int> BasisIndices) Solve(Matrix<double> A, Vector<double> b)
-        public static void Solve(double[,] A, double[] b)
+
+        private static Vector<double> CreateInitialSolutionVector(Vector<double> previous, int zeroCount)
         {
-            var _A = Matrix<double>.Build.DenseOfArray(A);
-            var _b = Vector<double>.Build.DenseOfArray(b);
-            
-            MinusOne(ref _A, _b);
-            
-            var extendedA = AppendIdentity(_A, CreateIdentityMatrix(_A.RowCount));
-            
-            var s = new SimplexMainPhaseService()
+            var zeros = Enumerable.Repeat<double>(element: 0, count: zeroCount); // n of 0's
+            var total = zeros.Concat(previous); // zeros + previous vector
+            return Vector<double>.Build.DenseOfEnumerable(total);
+        }
+
+        private static ISet<int> CreateInitialBasisIndices(int from, int count) =>
+            Enumerable.Range(from, count).ToHashSet();
+
+        // public static (Vector<double> X, ImmutableSortedSet<int> BasisIndices) Solve(Matrix<double> A, Vector<double> b)
+        public static void Solve(double[,] conditions, double[] constraints)
+        {
+            // ReSharper disable once InconsistentNaming
+            var A = Matrix<double>.Build.DenseOfArray(conditions);
+            var b = Vector<double>.Build.DenseOfArray(constraints);
+
+            MinusOne(ref A, b); // if i-th b's element less than zero, multiply i-th A's row by -1
+
+            var extended = AppendIdentity(A, CreateIdentityMatrix(A.RowCount)); // A|E
+            Debug.WriteLine($"extended:\n{extended.ToMatrixString()}\n");
+
+            var objectiveFunction = CreateObjectiveFunctionVector(A.ColumnCount, A.RowCount); // new c
+            Debug.WriteLine($"objectiveFunction:\n{objectiveFunction.ToVectorString()}\n");
+
+            var initialConditions = CreateInitialConditionsMatrix(A.ColumnCount, A.RowCount); // new A 
+            Debug.WriteLine($"initialConditions:\n{initialConditions.ToMatrixString()}\n");
+
+            var initialSolution = CreateInitialSolutionVector(b, A.ColumnCount); // new x
+            Debug.WriteLine($"initialSolution:\n{initialSolution.ToVectorString()}\n");
+
+            var initialBasisIndices = CreateInitialBasisIndices(A.ColumnCount, A.RowCount); // new J_b
+            Debug.WriteLine($"initialBasisIndices:\n{string.Join(", ", initialBasisIndices)}\n");
+
+            var mainPhase = new SimplexMainPhaseService(
+                initialConditions.ToArray(),
+                objectiveFunction.ToArray(),
+                initialSolution.ToArray(),
+                initialBasisIndices);
+            var result = mainPhase.Maximize();
         }
     }
 }
